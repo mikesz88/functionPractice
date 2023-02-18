0883451cc21174c1903700057d7b89ccc313fb44
michael <michaelsanchez8815@gmail.com>
2023-02-18 13:57:24 -0600

    finish exercise!

--- exercises/e3.js
+++ exercises/e3.js
@@ -14,8 +14,8 @@
  */
 
 // Your code goes here...
-
-
+const doesArrayIncludeItemsBetweenVals = (arr, val1, val2) =>
+  arr.some((num) => num > val1 && num < val2);
 
 /**
  * ====================================================
@@ -24,26 +24,34 @@
  * Arrow Function name: getValueWithConditionTwo;
  * Variable name for the function expression: getValueWithConditionThree;
  * All three functions resolve the same task:
-    * The function takes two numbers as arguments
-    * If the numbers are both equal to 40, return the sum of them
-    * Else return the doubled sum of them
+ * The function takes two numbers as arguments
+ * If the numbers are both equal to 40, return the sum of them
+ * Else return the doubled sum of them
  * getValueWithConditionOne(40, 40) => 80;
  * getValueWithConditionOne(20, 30) => 100;
  */
+function getValueWithConditionOne(num1, num2) {
+  return num1 === 40 && num2 === 40 ? num1 + num2 : (num1 + num2) * 2;
+}
 
+const getValueWithConditionTwo = (num1, num2) =>
+  num1 === 40 && num2 === 40 ? num1 + num2 : (num1 + num2) * 2;
 
+const getValueWithConditionThree = function (num1, num2) {
+  return num1 === 40 && num2 === 40 ? num1 + num2 : (num1 + num2) * 2;
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-3"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
-  doesArrayIncludeItemsBetweenVals: doesArrayIncludeItemsBetweenVals || undefined,
+  doesArrayIncludeItemsBetweenVals:
+    doesArrayIncludeItemsBetweenVals || undefined,
   getValueWithConditionOne: getValueWithConditionOne || undefined,
   getValueWithConditionTwo: getValueWithConditionTwo || undefined,
   getValueWithConditionThree: getValueWithConditionThree || undefined,
-}
+};
 export { f };
